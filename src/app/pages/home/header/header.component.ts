93d84efdaaab342d5b43026ff0d0bd58b847f589
Dawid Śremski <dawid.sremski@asseco.pl>
2020-02-11 19:02:07 +0100

    New branch for development

--- src/app/pages/home/header/header.component.ts
+++ src/app/pages/home/header/header.component.ts
@@ -17,7 +17,7 @@ export class HeaderComponent implements OnInit {
 
     const y: number = window.pageYOffset;
     this.heightVh = (100 - 0.2 * y >= 0) ? 100 - 0.2 * y : 0;
-    this.height = this.heightVh.toString() + 'vh';
+    // this.height = this.heightVh.toString() + 'vh';
     this.bgYPosPercent = (50 - 0.075 * y >= 0) ? 50 - 0.075 * y : 0;
     this.bgPos = '50% ' + this.bgYPosPercent + '%';
   }
