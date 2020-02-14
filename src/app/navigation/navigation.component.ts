a0a70cd14ab900a69e695968e3ae711c8654ef59
Dawid Śremski <dawid.sremski@gmail.com>
2020-02-14 10:32:44 +0100

    Simplify header.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
-import {Router} from '@angular/router';
+import {NavigationEnd, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -10,6 +10,14 @@ import {Router} from '@angular/router';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
+
+  constructor(private breakpointObserver: BreakpointObserver, public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.resetToolbarColor();
+      }
+    });
+  }
 
   menuItems = [
     {
@@ -26,12 +34,33 @@ export class NavigationComponent {
     },
   ];
 
+  initialOpacity = 0.25;
+  initialColor = `rgba(0,0,0, ${this.initialOpacity})`;
+  initialColorNoOpacity = 'rgba(0,0,0,1)';
+
+  opacity = this.initialOpacity;
+  toolbarColor = this.initialColor;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, public router: Router) {
+  resetToolbarColor() {
+    if (this.router.url === '/') {
+      this.toolbarColor = this.initialColor;
+    } else {
+      this.toolbarColor = this.initialColorNoOpacity;
+    }
+  }
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
+
+    if (this.router.url === '/') {
+      const y: number = window.pageYOffset;
+      this.opacity = (0.25 + 0.01 * y <= 1) ? 0.25 + 0.01 * y : 1;
+      this.toolbarColor = 'rgba(0,0,0,' + this.opacity.toString() + ')';
+    }
   }
 }
