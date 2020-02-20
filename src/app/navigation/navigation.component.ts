7a0ea6ebfd3c9037bf7ef3f8c6d76714588b8aa8
Dawid Śremski <dawid.sremski@asseco.pl>
2020-02-20 20:18:01 +0100

    Another visual changes. Add "partners" section.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,8 +1,8 @@
-import {Component, HostListener, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
-import {NavigationEnd, NavigationStart, Router} from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -12,11 +12,6 @@ import {NavigationEnd, NavigationStart, Router} from '@angular/router';
 export class NavigationComponent {
 
   constructor(private breakpointObserver: BreakpointObserver, public router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.resetToolbarColor();
-      }
-    });
   }
 
   menuItems = [
@@ -38,33 +33,9 @@ export class NavigationComponent {
     },
   ];
 
-  initialOpacity = 0.25;
-  initialColor = `rgba(0,0,0, ${this.initialOpacity})`;
-  initialColorNoOpacity = 'rgba(0,0,0,1)';
-
-  opacity = this.initialOpacity;
-  toolbarColor = this.initialColor;
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
-
-  resetToolbarColor() {
-    if (this.router.url === '/') {
-      this.toolbarColor = this.initialColor;
-    } else {
-      this.toolbarColor = this.initialColorNoOpacity;
-    }
-  }
-
-  @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
-
-    if (this.router.url === '/') {
-      const y: number = window.pageYOffset;
-      this.opacity = (0.25 + 0.01 * y <= 1) ? 0.25 + 0.01 * y : 1;
-      this.toolbarColor = 'rgba(0,0,0,' + this.opacity.toString() + ')';
-    }
-  }
 }
