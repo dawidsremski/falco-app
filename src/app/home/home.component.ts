dbc9860c1e1ea4057f22a90535b8a125118bd603
Dawid Śremski <dawid.sremski@gmail.com>
2020-03-19 13:00:49 +0100

    Add webp versions of backgrounds.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {ViewportScroller} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
 
--- src/app/sawmill/sawmill.component.ts
+++ src/app/sawmill/sawmill.component.ts
@@ -1,10 +1,11 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ViewEncapsulation} from '@angular/core';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-sawmill',
   templateUrl: './sawmill.component.html',
-  styleUrls: ['./sawmill.component.scss']
+  styleUrls: ['./sawmill.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SawmillComponent implements AfterViewInit {
 
